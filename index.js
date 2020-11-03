491364e2d7d041255bd51283b17c0d0bdaf68f70
I-Am-Lain <vetter.william@gmail.com>
2020-11-03 00:28:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, s){
+    return drivers.filter(d => {
+        return d.toLowerCase() === s.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, s){
+    return drivers.filter(d => {
+        return d.charAt(0).toLowerCase() === s.charAt(0).toLowerCase()
+    })
+}
+
+function matchName(drivers, s){
+    return drivers.filter(d => {
+        return d.name.toLowerCase() === s.toLowerCase()
+    })
+}
